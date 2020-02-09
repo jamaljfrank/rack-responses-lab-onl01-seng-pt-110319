e08a062e9d056b4d1bcc688fa046b1cddc9c3ae1
Jamal Frank <jamaljfrank@gmail.com>
2020-02-09 01:16:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
  
   def call(env)
-       (Time.now.to_i).between? ?  ["Good Morning!"] : ["Good Afternoon!"]
+       Time.now.to_i.between? ?  ["Good Morning!"] : ["Good Afternoon!"]
   end
  
 end
