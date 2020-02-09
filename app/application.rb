d3d60e5adf0573e24186b27bfaf7ed858df67713
Jamal Frank <jamaljfrank@gmail.com>
2020-02-09 01:15:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
  
   def call(env)
-       (Time.now.to_i % 2).zero? ?  ["Good Morning!"] : ["<strong>Hello</strong>"]
+       (Time.now.to_i % 2).zero? ?  ["Good Morning!"] : ["Good Afternoon!"]
   end
  
 end
